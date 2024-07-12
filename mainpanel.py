cb5e03091e01551d3862d5dd8e33977e5c5f1184
r-riveram <r.riveramanque@gmail.com>
2024-07-11 23:34:05 -0400

    ADD: PyQt6 designer.ui

--- mainpanel.py
+++ mainpanel.py
@@ -1,76 +1,52 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QLabel, QFileDialog
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
 import sys
 import mp3downloader
 import mp4downloader
+from designer import Ui_MainWindow 
 
-class MainWindow(QWidget):
+
+class MainWindow(QMainWindow, Ui_MainWindow): 
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('YouTube Downloader')
-        self.setGeometry(100, 100, 500, 200)
-
         self.destination_folder = None
-        layout_principal = QVBoxLayout()
-        layout_video = QHBoxLayout()
-        self.lbl_video = QLabel('Enlace del video:', self)
-        layout_video.addWidget(self.lbl_video)
-        self.txt_enlace = QLineEdit(self)
-        layout_video.addWidget(self.txt_enlace)
-        layout_principal.addLayout(layout_video)
 
-        layout_botones = QHBoxLayout()
-        btn_descargar_mp4 = QPushButton('Descargar MP4', self)
-        btn_descargar_mp4.clicked.connect(self.descargar_mp4)
-        layout_botones.addWidget(btn_descargar_mp4)
-
-        btn_descargar_mp3 = QPushButton('Descargar MP3', self)
-        btn_descargar_mp3.clicked.connect(self.descargar_mp3)
-        layout_botones.addWidget(btn_descargar_mp3)
-        layout_principal.addLayout(layout_botones)
-
-        btn_select_folder = QPushButton('Seleccionar ruta', self)
-        btn_select_folder.clicked.connect(self.select_folder)
-        layout_principal.addWidget(btn_select_folder)
-
-        self.lbl_estado = QLabel(self)
-        layout_principal.addWidget(self.lbl_estado)
-
-        self.lbl_folder = QLabel(self)
-        layout_principal.addWidget(self.lbl_folder)
-
-        self.setLayout(layout_principal)
-        self.show()
+        self.buttonmp4.clicked.connect(self.descargar_mp4)
+        self.buttonmp3.clicked.connect(self.descargar_mp3)
+        self.pushButton.clicked.connect(self.select_folder)
 
     def select_folder(self):
         folder = QFileDialog.getExistingDirectory(self, 'Seleccionar carpeta')
         if folder:
             self.destination_folder = folder
-            self.lbl_folder.setText(f'Carpeta seleccionada: {folder}')
+            self.statusbar.showMessage(f'Carpeta seleccionada: {folder}')
 
     def descargar_mp4(self):
-        enlace = self.txt_enlace.text()
+        enlace = self.url.toPlainText()
 
         if not self.destination_folder:
-            self.lbl_estado.setText('Por favor, seleccione una carpeta de destino.')
+            self.statusbar.showMessage('Por favor, seleccione una carpeta de destino.')
             return
 
         mensaje = mp4downloader.descargar_mp4(enlace, self.destination_folder)
-        self.lbl_estado.setText(mensaje)
+        self.statusbar.showMessage(mensaje)
 
     def descargar_mp3(self):
-        enlace = self.txt_enlace.text()
+        enlace = self.url.toPlainText()
 
         if not self.destination_folder:
-            self.lbl_estado.setText('Por favor, seleccione una carpeta de destino.')
+            self.statusbar.showMessage('Por favor, seleccione una carpeta de destino.')
             return
 
         mensaje = mp3downloader.descargar_mp3(enlace, self.destination_folder)
-        self.lbl_estado.setText(mensaje)
+        self.statusbar.showMessage(mensaje)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.show()
     sys.exit(app.exec())
